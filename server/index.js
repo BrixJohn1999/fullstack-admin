0017e1dfcc85c205e8af1381067702fbaf54f8f0
Brix John Novesteras <brixjohnovesteras@gmail.com>
2023-11-26 18:13:34 +0800

    connect mongoDB

--- server/index.js
+++ server/index.js
@@ -6,14 +6,13 @@ import dotenv from "dotenv";
 import helmet from "helmet";
 import morgan from "morgan";
 
-import clientRoutes from './routes/client.js'
+import clientRoutes from "./routes/client.js";
 import generalRoutes from "./routes/general.js";
 import managementRoutes from "./routes/management.js";
 import salesRoutes from "./routes/sales.js";
 
-
 /*CONFIGURATION */
-dotenv.use();
+dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(helmet());
@@ -27,4 +26,17 @@ app.use(cors());
 app.use("/client", clientRoutes);
 app.use("/general", generalRoutes);
 app.use("/management", managementRoutes);
-app.use("/sales", salesRoutes)
+app.use("/sales", salesRoutes);
+
+/*  MONGGOSE SETUP */
+const PORT = process.env.PORT || 9000;
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  })
+  .catch((error) => console.log(`${error} did not connect`));
