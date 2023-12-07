266356124c06decea51e3a4de6b03fed8dc703cb
brix john novesteras <brixjohnovesteras@gmail.com>
2023-12-07 11:00:25 +0800

    add product model

--- server/index.js
+++ server/index.js
@@ -13,7 +13,9 @@ import salesRoutes from "./routes/sales.js";
 
 // data import
 import User from "./models/User.js";
-import {dataUser} from "./data/index.js";
+import Product from './models/Product.js'
+import ProductStat from './models/ProductStat.js'
+import {dataUser, dataProduct, dataProductStat} from "./data/index.js";
 
 /*CONFIGURATION */
 dotenv.config();
@@ -43,7 +45,10 @@ mongoose
     app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
 
     /* ONLY ADD DATA ONE TIME*/
+    // Product.insertMany(dataProduct)
+    // ProductStat.insertMany(dataProductStat)
     // User.insertMany(dataUser);
+
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
